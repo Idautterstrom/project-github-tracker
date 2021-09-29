86a8332400ed0fecf3ff69754971abebd48d25e3
Idautterstrom <ida.utterstrom@hotmail.com>
2021-09-29 19:14:37 +0200

    APIs added

---
+++ code/script.js
@@ -0,0 +1,33 @@
+const API_KEY = "https://api.github.com/users/idautterstrom/repos"; //Endpoint to get all my repos
+const API_USER = "https://api.github.com/users/idautterstrom"; //Endpoint to my username and profile picture
+
+const projects = document.getElementById("projects");
+const user = document.getElementById("user")
+
+//To get the commits from a PR, you need to get the URL  from the commits_url property in the PR json object. 
+//It might look something like this:
+//https://api.github.com/repos/Technigo/project-news-site/pulls/227/commits
+//and then do a fetch with that url.
+
+
+fetch(API_USER) 
+.then((res) => res.json())
+.then((git_user) => {
+    console.log(git_user) //Mitt repo
+    user.innerHTML = `username: ${git_user.login} </br> <img src="https://avatars.githubusercontent.com/u/80784577?v=4" class="profile_picture" alt="profilepicture">`
+}) //kolla när hämtning 
+
+fetch(API_KEY) 
+   .then((res) => res.json())
+   .then((git_list) => {
+       console.log(git_list) //Mitt repo
+       git_list.forEach(repo => { //Börjat loopa igenom arrayen
+           console.log(repo.fork)
+           if (repo.fork === true) { //om ett specifikt värde
+               projects.innerHTML += `Name of repo: ${repo.name} </br> Last pushed: ${repo.pushed_at} </br> Branch: ${repo.default_branch} </br> Git url: ${repo.git_url} </br> Number of commits: ${repo.commits_url} </br>`  //formatting av $datum
+           }
+       });
+   })
+
+
+  
